e5d546945b65a4af9c5004ab400c234ca0494d17
samszo <samszon@gmail.com>
2025-08-29 15:21:23 +0200

    Ajout d'un script pour extraire un livre d'une image et rendre l'arrière-plan transparent.

---
+++ QweenImageEdit/test.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+import torch
+
+from diffusers import QwenImageEditPipeline
+
+pipeline = QwenImageEditPipeline.from_pretrained("Qwen/Qwen-Image-Edit")
+print("pipeline loaded")
+pipeline.to(torch.bfloat16)
+pipeline.to("cuda")
+pipeline.set_progress_bar_config(disable=None)
+image = Image.open("/Users/samszo/Sites/freloche/files/Photos-1-001/PXL_20250225_075603305.jpg").convert("RGB")
+prompt = "Extract the fist book from the image and make the background transparent, keeping only the person with a transparent background."
+inputs = {
+    "image": image,
+    "prompt": prompt,
+    "generator": torch.manual_seed(0),
+    "true_cfg_scale": 4.0,
+    "negative_prompt": " ",
+    "num_inference_steps": 50,
+}
+
+with torch.inference_mode():
+    output = pipeline(**inputs)
+    output_image = output.images[0]
+    output_image.save("output_image_edit.png")
+    print("image saved at", os.path.abspath("output_image_edit.png"))
